d7971f3b22fc7a11ec54d683c7fa437f8f5832ef
iswarkoki <ishwarkoki@gmail.com>
2025-05-04 21:04:38 +0530

    uvicorn path specified in pm2 configs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-    apps: [
-      {
-        name: "fundos-backend",
-        script: "uvicorn",
-        args: "src.main:app --host 0.0.0.0 --port 8000",
-        interpreter: "./.venv/bin/python3",
-        watch: false,
-        autorestart: true,
-        max_restarts: 5,
-        env: {
-          ENVIRONMENT: "production"
-        }
+  apps: [
+    {
+      name: "fundos-backend",
+      script: "./.venv/bin/uvicorn",
+      args: "src.main:app --host 0.0.0.0 --port 8000",
+      interpreter: "./.venv/bin/python3",
+      watch: false,
+      autorestart: true,
+      max_restarts: 5,
+      env: {
+        ENVIRONMENT: "production"
       }
-    ]
-  };
+    }
+  ]
+};
